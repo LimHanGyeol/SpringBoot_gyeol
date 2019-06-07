85b2b2941996086648689f789b679f4f55424b85
LimHanGyeol <dlagksruf19@naver.com>
2019-06-07 19:46:03 +0900

    initial commit

---
+++ gyeol_web1/src/main/java/com/example/gyeol_web1/GyeolWeb1Application.java
@@ -0,0 +1,13 @@
+package com.example.gyeol_web1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GyeolWeb1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GyeolWeb1Application.class, args);
+    }
+
+}
