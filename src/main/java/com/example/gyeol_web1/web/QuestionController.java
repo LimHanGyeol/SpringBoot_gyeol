976270c573c6a2e6c4540319d079fcb216ba3eeb
LimHanGyeol <dlagksruf19@naver.com>
2019-07-28 18:41:27 +0900

    스프링부트 qna 예제 정리1

--- src/main/java/com/example/gyeol_web1/web/QuestionController.java
+++ src/main/java/com/example/gyeol_web1/web/QuestionController.java
@@ -18,33 +18,42 @@ public class QuestionController {
     @Autowired
     private QuestionRepository questionRepository;
 
+    // /questions/form : 게시물 작성 화면
     @GetMapping("/form")
     public String form(HttpSession session) {
+        // 세션이 없으면 로그인 페이지로 가기
         if (!HttpSessionUtils.isLoginUsers(session)) {
             return "/users/loginForm";
         }
+        // 세션이 있으면 글 작성
         return "/qna/form";
     }
 
+    // 글 작성
     @PostMapping("")
     public String create(String title, String contents, HttpSession session) {
+        // 세션이 없으면 로그인 페이지로 가기
         if (!HttpSessionUtils.isLoginUsers(session)) {
             return "/users/loginForm";
         }
 
         User sessionedUser = HttpSessionUtils.getUsersFromSession(session);
         System.out.println("세션 확인 : " + sessionedUser);
+        // 세션이 있고, 제목과 내용을 newQuestion 객체로 매핑 하기
         Question newQuestion = new Question(sessionedUser, title, contents);
+        // question Table 에 저장
         questionRepository.save(newQuestion);
         return "redirect:/";
     }
 
+    // /questions/1 : 게시물 id 로 해당 id 의 게시물 보기
     @GetMapping("/{id}")
     public String show(@PathVariable Long id, Model model) {
         model.addAttribute("question", questionRepository.findById(id).get());
         return "/qna/show";
     }
 
+    // 게시글 수정 화면
     @GetMapping("/{id}/form")
     public String updateForm(@PathVariable Long id, Model model, HttpSession session) {
         Question question = questionRepository.findById(id).get();
@@ -57,6 +66,7 @@ public class QuestionController {
         return "/qna/updateForm";
     }
 
+    // 게시글 수정
     @PutMapping("/{id}")
     public String update(@PathVariable Long id, String title, String contents, Model model, HttpSession session) {
         Question question = questionRepository.findById(id).get();
@@ -70,6 +80,7 @@ public class QuestionController {
         return String.format("redirect:/questions/%d", id);
     }
 
+    // 게시글 삭제
     @DeleteMapping("/{id}")
     public String delete(@PathVariable Long id, Model model, HttpSession session) {
         Question question = questionRepository.findById(id).get();
--- src/main/java/com/example/gyeol_web1/web/UserController.java
+++ src/main/java/com/example/gyeol_web1/web/UserController.java
@@ -16,53 +16,67 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+    // 로그인 화면 으로 가기
     @GetMapping("/loginForm")
     public String loginForm() {
         return "/user/login";
     }
 
+    // 로그인
     @PostMapping("/login")
     public String login(String userId, String password, HttpSession session) {
+        // user Table 에 입력한 email 이 있는지 조회
         User user = userRepository.findByUserId(userId);
+
+        // 입력 값이 없을 경우 로그인 실패
         if (user == null) {
             System.out.println("Login Failed..");
             return "redirect:/users/loginForm";
         }
+
+        // 패스워드가 다를 경우 로그인 실패
         if (!user.matchPassword(password)) {
             System.out.println("Login Failed..");
             return "redirect:/users/loginForm";
         }
         System.out.println("Login Success!");
+
+        // 세션에 입력값 저장
         session.setAttribute(HttpSessionUtils.USER_SESSION_KEY,user);
 
         System.out.println("로그인시 세션 저장 : "+ session);
         return "redirect:/";
     }
 
+    // 로그아웃
     @GetMapping("/logout")
     public String logout(HttpSession session) {
         session.removeAttribute(HttpSessionUtils.USER_SESSION_KEY);
         return "redirect:/";
     }
 
+    // 회원가입 화면 이동
     @GetMapping("/form")
     public String form() {
         return "/user/form";
     }
 
+    // 회원가입
     @PostMapping("")
     public String create(User user) {
         System.out.println("user : " + user);
+        // 입력한 값으로 포스트 처리 하여 회원가입
         userRepository.save(user);
         return "redirect:/";
     }
 
+    // 회원 리스트 불러오기
     @GetMapping("")
     public String list(Model model) {
         model.addAttribute("users", userRepository.findAll());
         return "/user/list";
     }
-
+    // 회원 정보 수정 페이지로 가기
     @GetMapping("/{id}/form")
     public String updateForm(@PathVariable Long id, Model model, HttpSession session) {
         if (!HttpSessionUtils.isLoginUsers(session)) {
@@ -80,7 +94,7 @@ public class UserController {
         model.addAttribute("users", user);
         return "/user/updateForm";
     }
-
+    // 회원정보 수정
     @PutMapping("/{id}")
     public String update(@PathVariable Long id, User updatedUser, HttpSession session) {
         if (!HttpSessionUtils.isLoginUsers(session)) {
